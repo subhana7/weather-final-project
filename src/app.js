b1eaee0129dffbeb6352215cdbb49f20d6d14624
subhana7 <129241766+subhana7@users.noreply.github.com>
2023-04-07 17:16:44 -0400

    add current date

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
-let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-let city = "toronto";
-let units = "metric"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function FormatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0{hours}` ;
+}
+let minutes = data.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
 
 function showTemperature(response) {
     console.log(response);
@@ -15,12 +26,12 @@ function showTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = FormatDate(response.data.dt*1000);
 }
 
-
-
-
-
-
-
+let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+let city = "toronto";
+let units = "metric"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 axios.get(apiUrl).then(showTemperature);
