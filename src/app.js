d04c3c5308879b7899da6e8f9b9783e3ee59b88a
subhana7 <129241766+subhana7@users.noreply.github.com>
2023-04-07 16:02:15 -0400

    add real data to js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-let city = "new york";
+let city = "toronto";
 let units = "metric"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-console.log(apiUrl);
+
+function showTemperature(response) {
+    console.log(response);
+    let city = document.querySelector("#city");
+    city.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let tempElement = document.querySelector("#temp");
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+
+
+
+
+
+axios.get(apiUrl).then(showTemperature);
